6c887e255dd288f5879e9dfa6c3136802516d686
durgesh94 <durgesh.tambe94@gmail.com>
2022-05-01 20:43:06 +0530

    created: server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { PORT } = require('./config');
+
+//** Create express app */
+const app = express();
+
+//** CORS */
+app.use(cors());
+
+//** Parse application */
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//** Parse application/json */
+app.use(bodyParser.json());
+
+//** Define a sample route */
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to node application." });
+});
+
+//** Listen for requests */
+app.listen(PORT || 3000, () => {
+    console.log("Server is listening on port " + PORT || 3000);
+});
